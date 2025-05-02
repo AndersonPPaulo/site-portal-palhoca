b1006c9094998b3b1a8773dfdc1bbbb9070830f5
barcellos96 <felipebarcellos11@gmail.com>
2025-05-02 17:25:48 -0300

    feat: add breadcrumb component and integrate it into PostPage for improved navigation

--- src/components/posts/post-page.tsx
+++ src/components/posts/post-page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { mockPosts } from "@/utils/mock-data";
-import { ColumnistsSection } from "./columnists/columnist-section";
 import PostTopGridSection from "./sections/post-top-grid-section";
 import { useParams } from "next/navigation";
 import PostBanner from "../banner/post-banner";
@@ -11,6 +10,14 @@ import ButtonCTAWhatsAppButton from "../custom-button/cta-whatsapp-group-button"
 import { Share2 } from "lucide-react";
 import SideBanner from "../banner/side";
 import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../ui/breadcrumb";
 
 function normalizeText(text: string): string {
   return text
@@ -31,22 +38,38 @@ export default function PostPage() {
   const sidePosts = mockPosts.slice(0, 5);
   return (
     <section className="flex flex-col gap-6 mx-auto max-w-[1272px] justify-between">
+      <Breadcrumb className="font-semibold">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Portal</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink className="capitalize" href={`/noticia/${slug.name}`}>{slug.name}</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage className="capitalize text-primary font-semibold">{slug.slug}</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <div className="flex flex-col lg:flex-row">
         <div className="flex flex-col lg:flex-row w-full gap-2 rounded-2xl">
           {
             <div className="flex flex-col gap-3 rounded-xl p-2 transition">
               {/* aqui está autor, breadcrumb, categoria e compartilhar */}
+
               <div className="flex flex-col justify-between">
                 <h3 className="text-4xl max-w-[820px] font-[700] ">
-                  {postFiltered!.title}
+                  {postFiltered?.title}
                 </h3>
                 <div className="flex w-full justify-between">
                   <div className="flex items-center mt-2 gap-4">
                     <span className="w-min text-xs bg-secondary text-primary px-3 py-1 rounded-2xl">
-                      {postFiltered!.category}
+                      {postFiltered?.category}
                     </span>
                     <p className="text-xs text-gray-500">
-                      {postFiltered!.date}
+                      {postFiltered?.date}
                     </p>
 
                     <span className="flex gap-1 items-center hover:underline hover:cursor-pointer">
@@ -57,10 +80,10 @@ export default function PostPage() {
               </div>
 
               {/* imagem */}
-              <div className="relative max-w-[340px] lg:w-[840px] h-[475px] rounded-md overflow-hidden">
+              <div className="relative max-w-[340px] lg:max-w-[840px] h-[475px] rounded-md overflow-hidden">
                 <Image
-                  src={postFiltered!.image}
-                  alt={postFiltered!.title}
+                  src={postFiltered?.image ? postFiltered.image : ""}
+                  alt={postFiltered?.title ? postFiltered?.title : ""}
                   fill
                   className="object-cover "
                 />
@@ -75,7 +98,7 @@ export default function PostPage() {
 
               {/* conteudo */}
               <p className="text-[16px] text-[#363636] max-w-[840px] mb-10">
-                {postFiltered!.content}
+                {postFiltered?.content}
               </p>
               <ButtonCTAWhatsAppButton />
             </div>
